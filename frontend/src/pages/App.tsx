f8d1b20641c2b0ac1c3c483e5290365fe9184781
Jose Orihuela <jorihuel@pichincha.com>
2025-12-09 02:11:02 -0500

    fix: add missing routes /red, /ensenanzas, /events and imports for LaRed, Ensenanzas components

--- frontend/src/pages/App.tsx
+++ frontend/src/pages/App.tsx
@@ -3,6 +3,8 @@ import Landing from './Landing';
 import Login from './Login';
 import Register from './Register';
 import Unete from './Unete';
+import LaRed from './LaRed';
+import Ensenanzas from './Ensenanzas';
 import Dashboard from './Dashboard';
 import Courses from './Courses';
 import Events from './Events';
@@ -34,6 +36,9 @@ function AppInner() {
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/unete" element={<Unete />} />
+          <Route path="/red" element={<LaRed />} />
+          <Route path="/ensenanzas" element={<Ensenanzas />} />
+          <Route path="/events" element={<Events />} />
           <Route path="/dashboard" element={<ProtectedRoute><IntranetLayout><Dashboard /></IntranetLayout></ProtectedRoute>} />
           <Route path="/mentoria" element={<ProtectedRoute><IntranetLayout><Mentoria /></IntranetLayout></ProtectedRoute>} />
           <Route path="/oracion" element={<ProtectedRoute><IntranetLayout><Oracion /></IntranetLayout></ProtectedRoute>} />
